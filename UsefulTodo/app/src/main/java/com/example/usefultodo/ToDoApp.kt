c2492d67486f86deb599d89c69d4335d4a690d93
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-01 21:33:59 +0900

    initial commit

---
+++ UsefulTodo/app/src/main/java/com/example/usefultodo/ToDoApp.kt
@@ -0,0 +1,21 @@
+package com.example.usefultodo
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.usefultodo.ui.theme.UsefulTodoTheme
+
+@Composable
+fun ToDoApp(){
+    UsefulTodoTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Greeting("Android")
+        }
+    }
+}
