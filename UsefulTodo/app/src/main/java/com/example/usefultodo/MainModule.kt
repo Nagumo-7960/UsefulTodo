ba522fabc107313ce4a41ce1a42203731b1e06fe
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-05 22:15:07 +0900

    todoモデルの作成

--- UsefulTodo/app/src/main/java/com/example/usefultodo/MainModule.kt
+++ UsefulTodo/app/src/main/java/com/example/usefultodo/MainModule.kt
@@ -1,4 +1,32 @@
 package com.example.usefultodo
 
+import android.content.Context
+import androidx.room.Room
+import com.example.usefultodo.model.todo.ToDoDAO
+import com.example.usefultodo.model.todo.ToDoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object MainModule {
+    @Provides
+    @Singleton
+    fun provideToDoDatabase(@ApplicationContext context: Context): ToDoDatabase {
+        return Room.databaseBuilder(
+            context,
+            ToDoDatabase::class.java,
+            "todo.db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideToDoDAO(db: ToDoDatabase): ToDoDAO {
+        return db.todoDAO()
+    }
 }
