aba480b4f6c1aca7fed7915e5656fdacd4c8f70c
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-06 13:25:05 +0900

    viewModelインスタンスの作成

--- UsefulTodo/app/src/main/java/com/example/usefultodo/ToDoApp.kt
+++ UsefulTodo/app/src/main/java/com/example/usefultodo/ToDoApp.kt
@@ -1,13 +1,20 @@
 package com.example.usefultodo
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.usefultodo.ui.create.CreateToDoScreen
+import com.example.usefultodo.ui.create.CreateToDoViewModel
 import com.example.usefultodo.ui.detail.ToDoDetailScreen
+import com.example.usefultodo.ui.detail.ToDoDetailViewModel
 import com.example.usefultodo.ui.edit.EditToDoScreen
+import com.example.usefultodo.ui.edit.EditToDoViewModel
 import com.example.usefultodo.ui.main.MainScreen
+import com.example.usefultodo.ui.main.MainViewModel
 import com.example.usefultodo.ui.theme.UsefulTodoTheme
 
 @Composable
@@ -17,18 +24,36 @@ fun ToDoApp() {
     UsefulTodoTheme {
         NavHost(navController = navController, startDestination = "main") {
             composable("main") {
-                MainScreen(navController = navController)
+                val viewModel = hiltViewModel<MainViewModel>()
+                MainScreen(navController = navController, viewModel = viewModel)
             }
             composable("create") {
-                CreateToDoScreen(navController = navController)
+                val viewModel = hiltViewModel<CreateToDoViewModel>()
+                CreateToDoScreen(navController = navController, viewModel = viewModel)
             }
-            composable("detail/{todoId}") { backStackEntry ->
-                val todoId = backStackEntry.arguments?.getString("todoId")?.toInt() ?: 0
-                ToDoDetailScreen(navController = navController, todoId = todoId)
+            composable(
+                "detail/{todoId}",
+                arguments = listOf(navArgument("todoId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val viewModel = hiltViewModel<ToDoDetailViewModel>()
+                val todoId = backStackEntry.arguments?.getInt("todoId") ?: 0
+                ToDoDetailScreen(
+                    navController = navController,
+                    viewModel = viewModel,
+                    todoId = todoId
+                )
             }
-            composable("edit/{todoId}") { backStackEntry ->
-                val todoId = backStackEntry.arguments?.getString("todoId")?.toInt() ?: 0
-                EditToDoScreen(navController = navController, todoId = todoId)
+            composable(
+                "edit/{todoId}",
+                arguments = listOf(navArgument("todoId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val viewModel = hiltViewModel<EditToDoViewModel>()
+                val todoId = backStackEntry.arguments?.getInt("todoId") ?: 0
+                EditToDoScreen(
+                    navController = navController,
+                    viewModel = viewModel,
+                    todoId = todoId,
+                )
             }
         }
     }
