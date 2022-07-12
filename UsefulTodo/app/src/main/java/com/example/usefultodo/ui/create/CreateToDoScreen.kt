c565b126a0918a6bdfab4b7443c8bf4295e79edf
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-12 21:57:38 +0900

    CreateScreenのbodyを追加

--- UsefulTodo/app/src/main/java/com/example/usefultodo/ui/create/CreateToDoScreen.kt
+++ UsefulTodo/app/src/main/java/com/example/usefultodo/ui/create/CreateToDoScreen.kt
@@ -1,11 +1,49 @@
 package com.example.usefultodo.ui.create
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.usefultodo.R
 
 @Composable
 fun CreateToDoScreen(
     navController: NavController,
     viewModel: CreateToDoViewModel
 ) {
+
+}
+
+@Composable
+fun CreateToDoBody(
+    title: MutableState<String>,
+    detail: MutableState<String>,
+) {
+    Column {
+        TextField(
+            value = title.value,
+            onValueChange = { title.value = it },
+            label = { Text(stringResource(id = R.string.todo_title)) },
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+        TextField(
+            value = detail.value,
+            onValueChange = { detail.value = it },
+            label = { Text(stringResource(id = R.string.todo_detail)) },
+            modifier = Modifier
+                .weight(1.0f, true)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
 }
